f512e711ede08b3034318b882ea4faee4c712fa3
Jordan Klaers <jordan.klaers@ihsmarkit.com>
2019-11-08 14:26:28 -0700

    progress made on contrast challenge - koi fish (not done)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
 	<div id="app">
-		<Container></Container>
+		<!-- <challenge-18></challenge-18> -->
+		<component :is="this.components[1]"></component>
 	</div>
 </template>
 
 <script>
-import Container from './components/container'
+import Challenge18 from './components/Challenge18'
+import Contrast from './components/Codevember/contrast'
 import { mapGetters } from 'vuex'
 import store from './main.js'
 export default {
 	name: 'app',
 	components: {
-		Container
+		'challenge-18': Challenge18,
+		'contrast': Contrast
 	},
 	data() {
 		return {
-			count: 1
+			components: ['challenge-18', 'contrast']
 		}
 	},
 	computed: {
---
+++ src/components/Codevember/Contrast.vue
@@ -0,0 +1,71 @@
+<template>
+	<div class="container" id="contrast" ref="container">
+		<div class="fish-body">
+			<div class="head"></div>
+			<div class="body-1"></div>
+			<div class="body-2"></div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'contrast',
+	components: {
+	},
+	props: {},
+	data() {
+		return {
+		}
+	},
+	mounted() {
+	},
+	methods: {
+	},
+	watch: {
+	}
+};
+</script>
+<style lang='scss' type="text/scss">
+#contrast {
+	$white-fish: blue;
+	.fish-body {
+		bordeR: 1px solid black;
+		height: 100%;
+		width: 100%;
+		position: relative;
+		.head {
+			position: absolute;
+			height: 20%;
+			width: 23%;
+			background-color: $white-fish;
+			clip-path: ellipse(50% 60% at 50% 10%);
+			top: 45%;
+			left: 15%;
+			transform: rotate(15deg);
+		}
+		.body-1 {
+			position: absolute;
+			height: 20%;
+			width: 38%;
+			background-color: $white-fish;
+			clip-path: ellipse(55% 60% at 50% 100%);
+			top: 20%;
+			left: 10%;
+			transform: rotate(-40deg);
+		}
+		.body-2 {
+			position: absolute;
+			height: 15%;
+			width: 25%;
+			background-color: $white-fish;
+			clip-path: ellipse(80% 60% at 50% 100%);
+			top: 14%;
+			left: 40%;
+		}
+	}
+}
+
+</style>
+
+
